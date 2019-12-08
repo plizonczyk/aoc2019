a77e8303c16d5d6ce333b1a72d072232266f74f3
Piotr Lizonczyk <piotr.lizonczyk@appdynamics.com>
2019-12-07 22:48:43 -0800

    d8p1

---
+++ src/main/python/d8.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+
+class Solver:
+    def __init__(self):
+        with open("../resources/d8/input") as f:
+            self.data = f.readline().strip()
+
+    def solveP1(self, width, height):
+        layers = [self.data[i:i+width*height] for i in range(0, len(self.data), width*height)]
+        min_layer = min(layers, key=lambda x: Counter(x).get('0', 0))
+        print(Counter(min_layer))
+
+
+if __name__ == '__main__':
+    print('P1')
+    s = Solver()
+    s.solveP1(25, 6)
+
+    # print('P2')
+    # s = Solver()
+    # s.solveP2()
+
