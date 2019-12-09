5943d29a4b4338bafabde95c9d090d51c7edaf5d
Piotr Lizonczyk <piotr.lizonczyk@appdynamics.com>
2019-12-08 23:06:13 -0800

    intcode machine refactored

--- src/main/python/d9.py
+++ src/main/python/d9.py
@@ -1,23 +1,31 @@
-from collections import defaultdict
-
-
 class Solver:
-    def __init__(self):
-        self.data = defaultdict(int)
+    def __init__(self, inp):
         with open("../resources/d9/input") as f:
             data = map(lambda x: int(x), f.readline().split(','))
-        self.data.update({i: value for i, value in enumerate(data)})
+        self.data = dict(enumerate(data))
         self.relative_base = 0
         self.pc = 0
+        self.input = inp
         self.flags = None
 
-    def parse_opcode(self, opcode):
-        code = int(opcode[-2:])
-        flags = [0, 0, 0]
-        for i, flag in enumerate(opcode[-3::-1]):
-            flags[i] = int(flag)
+        self.opcode_funs = {
+            1: self.addition,
+            2: self.multiplication,
+            3: self.read_input,
+            4: self.print_output,
+            5: self.jmp_notzero,
+            6: self.jmp_zero,
+            7: self.less_than,
+            8: self.equals,
+            9: self.set_relative_base
+        }
 
-        return code, flags
+    def parse_opcode(self, opcode):
+        self.flags = [0, 0, 0]
+        for i, flag in enumerate(opcode[-3::-1]):
+            self.flags[i] = int(flag)
+
+        return int(opcode[-2:])  # method code
 
     def get_ptr(self, arg_position):
         if self.flags[arg_position - 1] == 0:  # positional
@@ -30,53 +38,57 @@ class Solver:
     def get_val(self, arg_position):
         return self.data[self.get_ptr(arg_position)]
 
-    def solve(self, inp):
+    def solve(self):
         while self.pc < len(self.data):
-            method, self.flags = self.parse_opcode(str(self.data[self.pc]))
-            if method == 1:  # addition
-                self.data[self.get_ptr(3)] = self.get_val(1) + self.get_val(2)
-                self.pc += 4
-            elif method == 2:  # multiplication
-                self.data[self.get_ptr(3)] = self.get_val(1) * self.get_val(2)
-                self.pc += 4
-            elif method == 3:  # read input
-                self.data[self.get_ptr(1)] = inp
-                self.pc += 2
-            elif method == 4:  # print output
-                print(self.data[self.get_ptr(1)])
-                self.pc += 2
-            elif method == 5:  # jmp if not zero
-                if self.data[self.get_ptr(1)] != 0:
-                    self.pc = self.data[self.get_ptr(2)]
-                else:
-                    self.pc += 3
-            elif method == 6:  # jmp if zero
-                if self.data[self.get_ptr(1)] == 0:
-                    self.pc = self.data[self.get_ptr( 2)]
-                else:
-                    self.pc += 3
-            elif method == 7:  # arg1 less than arg2
-                self.data[self.get_ptr(3)] = 1 if self.get_val(1) < self.get_val(2) else 0
-                self.pc += 4
-            elif method == 8:  # arg1 equals arg2
-                self.data[self.get_ptr(3)] = 1 if self.get_val(1) == self.get_val(2) else 0
-                self.pc += 4
-            elif method == 9:  # set relative base
-                self.relative_base += self.get_val(1)
-                self.pc += 2
-            elif method == 99:  # halt
+            method = self.parse_opcode(str(self.data[self.pc]))
+            if method == 99:  # halt
                 return
+            self.opcode_funs[method]()
 
-    #
-    # def solveP2(self):
-    #     print(self.minimum_steps(self.lines))
+    def addition(self):
+        self.data[self.get_ptr(3)] = self.get_val(1) + self.get_val(2)
+        self.pc += 4
+
+    def multiplication(self):
+        self.data[self.get_ptr(3)] = self.get_val(1) * self.get_val(2)
+        self.pc += 4
+
+    def read_input(self):
+        self.data[self.get_ptr(1)] = self.input
+        self.pc += 2
+
+    def print_output(self):
+        print(self.data[self.get_ptr(1)])
+        self.pc += 2
+
+    def jmp_notzero(self):
+        if self.data[self.get_ptr(1)] != 0:
+            self.pc = self.data[self.get_ptr(2)]
+        else:
+            self.pc += 3
+
+    def jmp_zero(self):
+        if self.data[self.get_ptr(1)] == 0:
+            self.pc = self.data[self.get_ptr(2)]
+        else:
+            self.pc += 3
+
+    def less_than(self):
+        self.data[self.get_ptr(3)] = 1 if self.get_val(1) < self.get_val(2) else 0
+        self.pc += 4
+
+    def equals(self):
+        self.data[self.get_ptr(3)] = 1 if self.get_val(1) == self.get_val(2) else 0
+        self.pc += 4
+
+    def set_relative_base(self):
+        self.relative_base += self.get_val(1)
+        self.pc += 2
 
 
 if __name__ == '__main__':
     print('P1')
-    s = Solver()
-    s.solve(1)
+    Solver(1).solve()
 
     print('P2')
-    s = Solver()
-    s.solve(2)
+    Solver(2).solve()
